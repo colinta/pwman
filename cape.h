a162451c9c3ed9508a126c7719cd4b61661c9f99
Colin T.A. Gray <colinta@gmail.com>
2018-01-03 11:50:44 -0700

    refactors and adds encryption

---
+++ cape.h
@@ -0,0 +1,66 @@
+/*
+    _____  _____   _____   _____
+   |      |_____| |_____| |_____
+   |_____ |     | |       |_____  version 2.0
+
+Cape Copyright (c) 2012-2017, Giovanni Blu Mitolo All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#pragma once
+
+typedef struct {
+    char salt; // Salt used for encryption, can be exchanged if encrypted
+    char *key;              //
+    uint16_t length;    // Keep these private and safe
+    char reduced_key;       //
+} cape_t;
+
+void cape_init(cape_t *cape, char *key, uint16_t length, uint8_t s);
+void cape_crypt(cape_t *cape, char *source, char *destination, uint16_t length);
+bool process_record_cape(char keycode, cape_t *cape, char *cape_password, uint16_t *cape_password_length, uint16_t salt);
+
+/// CAPE
+
+/* Compute a 1 byte version of the encryption key */
+uint16_t cape_compute_reduced_key(char *key, uint16_t length) {
+    uint16_t reduced_key = 0;
+    // Reduced key computation
+    for(uint16_t i = 0; i < length; i++) {
+        reduced_key ^= (key[i] << (i % 8));
+    }
+    return reduced_key;
+}
+
+void cape_init(cape_t *cape, char *key, uint16_t length, uint8_t salt) {
+    cape->salt = salt;
+    cape->key = key;
+    cape->length = length;
+    cape->reduced_key = cape_compute_reduced_key(key, length);
+}
+
+/* Symmetric chiper using private key, reduced key and optionally salt:
+   (max 65535 characters) */
+void cape_crypt(cape_t *cape,
+    char *source,
+    char *destination,
+    uint16_t length)
+{
+    for(uint16_t i = 0; i < length; i++) {
+        destination[i] = (
+            (cape->reduced_key ^ source[i] ^ cape->salt ^ i) ^
+            cape->key[(cape->reduced_key ^ cape->salt ^ i) % cape->length]
+        );
+    }
+    destination[length] = 0;
+}
--- pwman.ino
+++ pwman.ino
@@ -1,16 +1,14 @@
+#define PWMAN_VERSION 2
+
 #include <math.h>
 #include <Arduboy2.h>
 #include <Keyboard.h>
+#include "cape.h"
 
-#define READ 0
-#define WRITE_NAME 1
-#define WRITE_PW 2
+#define KC_ENTER 10
 #define FRAME_RATE 60
-#define MAX_ENTRIES 8
-#define MAX_LEN 255
 
 Arduboy2 arduboy;
-uint8_t selectedEntry = 0;
 
 struct PasswordEntry {
   char *name;
@@ -19,8 +17,25 @@ struct PasswordEntry {
 };
 
 int serial = 0;
-uint8_t appState = READ;
-char write_bfr[MAX_LEN + 1];
+
+#define STATE_NONE 0
+#define STATE_VERIFY 1
+#define STATE_MAIN 2
+#define STATE_WRITE_NAME 3
+#define STATE_WRITE_PW 4
+#define STATE_OUTPUT 5
+uint8_t appState = STATE_NONE;
+uint8_t nextAppState = STATE_VERIFY;
+
+#define MAX_ENTRIES 8
+#define MAX_NAME_LEN 20
+#define MAX_PASSWORD_LEN 100
+#define MAX_UNLOCK_LEN 10
+cape_t cape;
+uint8_t cape_salt = 0;
+uint8_t selectedEntry = 0;
+char verifyPassword[] = {0,0,0,0,0,0,0,0,0,0,0};
+char buffer[MAX_PASSWORD_LEN + 1];
 PasswordEntry entries[] = {
   { .name = NULL, .pw = NULL, .pwLen = 0 },
   { .name = NULL, .pw = NULL, .pwLen = 0 },
@@ -68,53 +83,99 @@ void loop() {
   }
 
   arduboy.clear();
-  if (appState == READ) {
-    readLoop();
+  if (nextAppState != appState) {
+    switch (nextAppState) {
+      case STATE_VERIFY:
+        switchToVerify();
+        break;
+      case STATE_MAIN:
+        switchToMain();
+        break;
+      case STATE_WRITE_NAME:
+        switchToWrite();
+        break;
+      case STATE_WRITE_PW:
+        // no-op
+        break;
+      case STATE_OUTPUT:
+        switchToOutput();
+        break;
+    }
+
+    appState = nextAppState;
   }
-  else if (appState == WRITE_NAME || appState == WRITE_PW) {
-    writeLoop();
+
+  switch (appState) {
+    case STATE_VERIFY:
+      verifyLoop();
+      break;
+    case STATE_MAIN:
+      mainLoop();
+      break;
+    case STATE_WRITE_NAME:
+    case STATE_WRITE_PW:
+      writeLoop();
+      break;
+    case STATE_OUTPUT:
+      outputLoop();
+      break;
   }
+
   arduboy.display();
 }
 
-void writeLoop() {
-  if (!serial) { return; }
+void switchToVerify() {}
+void switchToMain() {}
+void switchToWrite() {
+  PasswordEntry *entry = &entries[selectedEntry];
+  resetEntry(entry);
+  resetBuffer();
+  Serial.print("Entry name: ");
+}
+void switchToOutput() {}
 
-  if (serial >= 32 && serial < 127 && strlen(write_bfr) < MAX_LEN) {
-    write_bfr[strlen(write_bfr)] = (char)serial;
-    if (appState == WRITE_NAME) {
-      Serial.print((char)serial);
+void verifyLoop() {
+  if (arduboy.justPressed(A_BUTTON)) {
+    for (uint8_t i = 0; i < MAX_UNLOCK_LEN; ++i) {
+      verifyPassword[i] = 0;
     }
   }
-  else if (serial == 13) {
-    PasswordEntry *entry = &entries[selectedEntry];
-    if (appState == WRITE_NAME) {
-      Serial.print("\r\nCopied \"");
-      Serial.print(write_bfr);
-      Serial.print("\"");
-      entry->name = (char*)malloc(sizeof(char) * (strlen(write_bfr) + 1));
-      strcpy(entry->name, write_bfr);
 
-      appState = WRITE_PW;
-      Serial.print("\r\nEnter the password:");
-      resetWriteBfr();
-    }
-    else {
-      Serial.print("\r\nCopied \"");
-      Serial.print(write_bfr);
-      Serial.print("\"");
-      entry->pw = (char*)malloc(sizeof(char) * (strlen(write_bfr) + 1));
-      strcpy(entry->pw, write_bfr);
-      entry->pwLen = strlen(write_bfr);
+  if (arduboy.justPressed(B_BUTTON)) {
+    cape_init(&cape, verifyPassword, strlen(verifyPassword), cape_salt);
+    nextAppState = STATE_MAIN;
+  }
 
-      appState = READ;
-      Serial.print("\r\ndone.\r\n");
-      pwmanSave();
+  char pressed = 0;
+  if (arduboy.justPressed(UP_BUTTON)) {
+    pressed = 'a';
+  }
+  else if (arduboy.justPressed(DOWN_BUTTON)) {
+    pressed = 'b';
+  }
+  else if (arduboy.justPressed(LEFT_BUTTON)) {
+    pressed = 'c';
+  }
+  else if (arduboy.justPressed(RIGHT_BUTTON)) {
+    pressed = 'd';
+  }
+
+  if (pressed && strlen(verifyPassword) < MAX_UNLOCK_LEN) {
+    verifyPassword[strlen(verifyPassword)] = pressed;
+  }
+
+  arduboy.setCursor(0, 8*selectedEntry);
+  if (strlen(verifyPassword)) {
+    for (uint8_t i = 0; i < strlen(verifyPassword); ++i) {
+      arduboy.print("*");
     }
+  }
+  else {
+    arduboy.print("_");
   }
 }
 
-void readLoop() {
+void mainLoop() {
   if (arduboy.justPressed(UP_BUTTON) && selectedEntry > 0) {
     selectedEntry -= 1;
   }
@@ -124,28 +185,11 @@ void readLoop() {
   }
 
   if (arduboy.justPressed(B_BUTTON)) {
-    PasswordEntry *entry = &entries[selectedEntry];
-    if (entry->pw) {
-      Keyboard.begin();
-      for (uint8_t entry_i = 0 ; entry_i < entry->pwLen ; ++entry_i) {
-        char key = entry->pw[entry_i];
-        Keyboard.press(key);
-        delay(10);
-        Keyboard.release(key);
-      }
-      Keyboard.press(10);
-      delay(10);
-      Keyboard.release(10);
-      Keyboard.end();
-    }
+    nextAppState = STATE_OUTPUT;
   }
 
   if (arduboy.justPressed(A_BUTTON)) {
-    PasswordEntry *entry = &entries[selectedEntry];
-    resetEntry(entry);
-    resetWriteBfr();
-    appState = WRITE_NAME;
-    Serial.print("Entry name: ");
+    nextAppState = STATE_WRITE_NAME;
   }
 
   arduboy.setTextSize(1);
@@ -160,20 +204,64 @@ void readLoop() {
   }
 }
 
-// this check is a way to have some confidence that the EEPROM was under this
-// game's control last time it was accessed.
-bool checkEEPROM() {
-  if ( EEPROM.read(0) != 'P' ) return false;
-  if ( EEPROM.read(1) != 'W' ) return false;
-  if ( EEPROM.read(2) != 'M' ) return false;
-  if ( EEPROM.read(3) != 'A' ) return false;
-  if ( EEPROM.read(4) != 'N' ) return false;
-  return true;
+void writeLoop() {
+  if (!serial) { return; }
+
+  if (serial >= 32 && serial < 127 && (nextAppState == STATE_WRITE_NAME ? strlen(buffer) < MAX_NAME_LEN : strlen(buffer) < MAX_PASSWORD_LEN)) {
+    buffer[strlen(buffer)] = (char)serial;
+    if (nextAppState == STATE_WRITE_NAME) {
+      Serial.print((char)serial);
+    }
+  }
+  else if (serial == 13) {
+    PasswordEntry *entry = &entries[selectedEntry];
+    if (nextAppState == STATE_WRITE_NAME) {
+      entry->name = (char*)malloc(sizeof(char) * (strlen(buffer) + 1));
+      strcpy(entry->name, buffer);
+
+      nextAppState = STATE_WRITE_PW;
+      Serial.print("\r\nEnter the password:");
+      resetBuffer();
+    }
+    else {
+      entry->pwLen = strlen(buffer);
+      entry->pw = (char*)malloc(sizeof(char) * (strlen(buffer) + 1));
+      cape_crypt(&cape, buffer, entry->pw, entry->pwLen);
+
+      nextAppState = STATE_MAIN;
+      Serial.println("\r\n");
+      pwmanSave();
+    }
+  }
 }
 
-void resetWriteBfr() {
-  for (uint16_t i = 0 ; i < MAX_LEN + 1 ; ++i) {
-    write_bfr[i] = 0;
+void outputLoop() {
+  PasswordEntry *entry = &entries[selectedEntry];
+  if (entry->pw) {
+    cape_crypt(&cape, entry->pw, buffer, entry->pwLen);
+
+    Keyboard.begin();
+    for (uint8_t entry_i = 0 ; entry_i < entry->pwLen ; ++entry_i) {
+      char key = buffer[entry_i];
+
+      if (key >= ' ' && key <= '~') {
+        Keyboard.press(key);
+        delay(10);
+        Keyboard.release(key);
+      }
+    }
+    Keyboard.press(KC_ENTER);
+    delay(10);
+    Keyboard.release(KC_ENTER);
+    Keyboard.end();
+    resetBuffer();
+  }
+  nextAppState = STATE_MAIN;
+}
+
+void resetBuffer() {
+  for (uint16_t i = 0 ; i < MAX_PASSWORD_LEN + 1 ; ++i) {
+    buffer[i] = 0;
   }
 }
 
@@ -199,9 +287,9 @@ void pwmanInit() {
     entry->pwLen = 0;
   }
 
-  if (!checkEEPROM()) { return; }
+  uint16_t eeprom_ptr = 0;
+  if (!checkEEPROM(&eeprom_ptr)) { return; }
 
-  uint16_t eeprom_ptr = 5;
   uint8_t name_len = 0;
   char *str;
 
@@ -233,13 +321,14 @@ void pwmanInit() {
 }
 
 void pwmanSave() {
-  EEPROM.update(0, 'P');
-  EEPROM.update(1, 'W');
-  EEPROM.update(2, 'M');
-  EEPROM.update(3, 'A');
-  EEPROM.update(4, 'N');
+  uint16_t eeprom_ptr = EEPROM_STORAGE_SPACE_START;
+  EEPROM.update(eeprom_ptr++, 'P');
+  EEPROM.update(eeprom_ptr++, 'W');
+  EEPROM.update(eeprom_ptr++, 'm');
+  EEPROM.update(eeprom_ptr++, 'a');
+  EEPROM.update(eeprom_ptr++, 'n');
+  EEPROM.update(eeprom_ptr++, PWMAN_VERSION);
 
-  uint16_t eeprom_ptr = 5;
   for (uint8_t i = 0 ; i < MAX_ENTRIES ; ++i ) {
     PasswordEntry *entry = &entries[i];
     if (!entry->name) {
@@ -258,4 +347,20 @@ void pwmanSave() {
       EEPROM.update(eeprom_ptr++, entry->pw[i]);
     }
   }
+}
+
+// this check is a way to have some confidence that the EEPROM was under this
+// game's control last time it was accessed.
+bool checkEEPROM(uint16_t *eeprom_ptr) {
+  uint8_t ptr = EEPROM_STORAGE_SPACE_START;
+  if ( EEPROM.read(ptr++) != 'P' ) return false;
+  if ( EEPROM.read(ptr++) != 'W' ) return false;
+  if ( EEPROM.read(ptr++) != 'm' ) return false;
+  if ( EEPROM.read(ptr++) != 'a' ) return false;
+  if ( EEPROM.read(ptr++) != 'n' ) return false;
+  if ( EEPROM.read(ptr++) != PWMAN_VERSION ) return false;
+  if (eeprom_ptr) {
+    *eeprom_ptr = ptr;
+  }
+  return true;
 }
